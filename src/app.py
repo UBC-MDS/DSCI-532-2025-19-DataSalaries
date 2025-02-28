bf15620415f3ce176de51598330d65d8f5745a43
jessiezhang24 <zhangj24@student.ubc.ca>
2025-02-28 11:30:20 -0800

    fix: edit wording for clarity

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 # Components
 title = html.H1(
-    '💰DataSalaries - Data Science Salary Insights Dashboard',
+    '💰DataSalaries - Salary Insights for Data Professionals',
     style={
         'backgroundColor': 'steelblue',
         'padding': 20,
@@ -104,10 +104,10 @@ sidebar = dbc.Col([
 footer = html.Footer(
     [
         html.Hr(), 
-        html.P("DataSalaries Dashboard designed to analyze and visualize salary trends in the data science field 📊", style={'font-size': '14px'}),
-        html.P("Created by Group 19 - Jessie Zhang, Tianjiao Jiang, Rashid Mammadov, Karlygash Zhakupbayeva | ", style={'font-size': '14px', 'display': 'inline'}),
-        html.A("GitHub Repo", href="https://github.com/UBC-MDS/DSCI-532-2025-19-DataSalaries", target="_blank", style={'font-size': '14px', 'color': 'blue', 'display': 'inline'}),
-        html.P(" | Last updated: February 28, 2025", style={'font-size': '14px', 'display': 'inline'}),
+        html.P("📊DataSalaries Dashboard designed to provide salary trends and insights across various data-related roles by visualizing salary based on job title, experience level, and employment details!", style={'font-size': '14px'}),
+        html.P("👨‍💻Created by Group 19 - Jessie Zhang, Tianjiao Jiang, Rashid Mammadov, Karlygash Zhakupbayeva | ", style={'font-size': '14px', 'display': 'inline'}),
+        html.A("🔗GitHub Repo", href="https://github.com/UBC-MDS/DSCI-532-2025-19-DataSalaries", target="_blank", style={'font-size': '14px', 'color': 'blue', 'display': 'inline'}),
+        html.P(" | 📅Last updated: February 28, 2025", style={'font-size': '14px', 'display': 'inline'}),
     ],
     style={
         'text-align': 'center',
